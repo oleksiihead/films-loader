6691bb76ebe83799a4acf0ff2a11e9b8dd88b7cf
Oleksii Smiichuk <smiychuk@gmail.com>
2022-02-10 21:57:54 +0200

    Fix according to PEP8

--- test_main.py
+++ test_main.py
@@ -19,5 +19,6 @@ class TestJSONLoaderMethods(unittest.TestCase):
     def test_id(self):
         self.assertEqual(self.movies[0]['id'], 'tt0111161')
 
+
 if __name__ == '__main__':
     unittest.main()
